d5c5be868691e97aa2ad121a1579ac7a1673851f
Luxcih <luxcih.me@gmail.com>
2026-08-30 00:27:21 +0800

    Polish core Neovim API helpers

--- lua/core/command.lua
+++ lua/core/command.lua
@@ -1,5 +1,17 @@
 local M = {}
 
-M.create = vim.api.nvim_create_user_command
+--- Create a user command.
+---@param name string
+---@param command string|function
+---@param opts? vim.api.keyset.user_command
+function M.create(name, command, opts)
+  vim.api.nvim_create_user_command(name, command, opts or {})
+end
+
+--- Delete a user command.
+---@param name string
+function M.delete(name)
+  vim.api.nvim_del_user_command(name)
+end
 
 return M
