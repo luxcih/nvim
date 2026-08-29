fadbaebef32cf591d03c208130f5ad9155c9002a
Luxcih <luxcih.me@gmail.com>
2026-08-30 00:27:25 +0800

    Polish core Neovim API helpers

--- lua/core/keymap.lua
+++ lua/core/keymap.lua
@@ -1,5 +1,20 @@
 local M = {}
 
-M.set = vim.keymap.set
+--- Set a keymap.
+---@param mode string|string[]
+---@param lhs string
+---@param rhs string|function
+---@param opts? vim.keymap.set.Opts
+function M.set(mode, lhs, rhs, opts)
+  vim.keymap.set(mode, lhs, rhs, opts or {})
+end
+
+--- Delete a keymap.
+---@param mode string|string[]
+---@param lhs string
+---@param opts? vim.keymap.del.Opts
+function M.delete(mode, lhs, opts)
+  vim.keymap.del(mode, lhs, opts or {})
+end
 
 return M
