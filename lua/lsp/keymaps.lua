291874a860434a4fe5424a17c85664dc323da494
Luxcih <luxcih.me@gmail.com>
2026-08-30 09:30:28 +0800

    Simplify buffer-local LSP keymaps

--- lua/lsp/keymaps.lua
+++ lua/lsp/keymaps.lua
@@ -4,36 +4,30 @@ local extra = require("mini.extra")
 
 local group = autocmd.group("Lsp", { clear = true })
 
+local function map(buf, lhs, rhs, desc)
+  keymap.set("n", lhs, rhs, {
+    buffer = buf,
+    desc = desc,
+  })
+end
+
 autocmd.create("LspAttach", {
   group = group,
   callback = function(event)
-    local opts = { buffer = event.buf }
+    local buf = event.buf
 
-    keymap.set("n", "gd", vim.lsp.buf.definition, vim.tbl_extend("force", opts, {
-      desc = "Goto Definition",
-    }))
-    keymap.set("n", "gD", vim.lsp.buf.declaration, vim.tbl_extend("force", opts, {
-      desc = "Goto Declaration",
-    }))
-    keymap.set("n", "gi", vim.lsp.buf.implementation, vim.tbl_extend("force", opts, {
-      desc = "Goto Implementation",
-    }))
-    keymap.set("n", "gr", function()
+    map(buf, "gd", vim.lsp.buf.definition, "Goto Definition")
+    map(buf, "gD", vim.lsp.buf.declaration, "Goto Declaration")
+    map(buf, "gi", vim.lsp.buf.implementation, "Goto Implementation")
+
+    map(buf, "gr", function()
       extra.pickers.lsp({ scope = "references" })
-    end, vim.tbl_extend("force", opts, {
-      desc = "References",
-    }))
-    keymap.set("n", "K", vim.lsp.buf.hover, vim.tbl_extend("force", opts, {
-      desc = "Hover",
-    }))
-    keymap.set("n", "gK", vim.lsp.buf.signature_help, vim.tbl_extend("force", opts, {
-      desc = "Signature Help",
-    }))
-    keymap.set("n", "<Leader>rn", vim.lsp.buf.rename, vim.tbl_extend("force", opts, {
-      desc = "Rename",
-    }))
-    keymap.set("n", "<Leader>ca", vim.lsp.buf.code_action, vim.tbl_extend("force", opts, {
-      desc = "Code Action",
-    }))
+    end, "References")
+
+    map(buf, "K", vim.lsp.buf.hover, "Hover")
+    map(buf, "gK", vim.lsp.buf.signature_help, "Signature Help")
+
+    map(buf, "<Leader>rn", vim.lsp.buf.rename, "Rename")
+    map(buf, "<Leader>ca", vim.lsp.buf.code_action, "Code Action")
   end,
 })
